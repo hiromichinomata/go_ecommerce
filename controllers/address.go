4e1be94ff2f0dd7cb350a0ab4b0a34ab79b63ddb
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-28 17:58:13 +0900

    add: address, cart boilerplate

--- controllers/address.go
+++ controllers/address.go
@@ -1,1 +1,27 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
+
+func EditHomeAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
+
+func EditWorkAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
+
+func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
--- controllers/cart.go
+++ controllers/cart.go
@@ -1,1 +1,33 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddToCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
+
+func RemoteItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
+
+func GetItemFromCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
+
+func BuyFromCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
+
+func InstantBuy() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dummy"})
+	}
+}
