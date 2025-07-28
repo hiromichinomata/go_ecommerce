e09e331b22ef9b084d97ddb4a654ce950da12b94
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-29 00:44:30 +0900

    add: remove item

--- controllers/cart.go
+++ controllers/cart.go
@@ -55,9 +55,37 @@ func (app *Application) AddToCart() gin.HandlerFunc {
 		c.IndentedJSON(200, "Successfully Added to the cart")
 	}
 }
-func RemoteItem() gin.HandlerFunc {
+
+func (app *Application) RemoveItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "dummy"})
+		productQueryID := c.Query("id")
+		if productQueryID == "" {
+			log.Println("product id is inavalid")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
+			return
+		}
+
+		userQueryID := c.Query("userID")
+		if userQueryID == "" {
+			log.Println("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("UserID is empty"))
+		}
+
+		ProductID, err := primitive.ObjectIDFromHex(productQueryID)
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		err = database.RemoveCartItem(ctx, app.prodCollection, app.userCollection, ProductID, userQueryID)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+			return
+		}
+		c.IndentedJSON(200, "Successfully removed from cart")
 	}
 }
 
